d6c519a0b23f8c2476e969e8350fcf400ce9b87d
Duc <tanghuynh09@gmail.com>
2025-06-17 16:23:39 +0700

    Xác thực cấp người dùng

---
+++ src/auth/roles/roles.decorator.ts
@@ -0,0 +1,7 @@
+
+import { SetMetadata } from '@nestjs/common';
+import { Role } from '../../enums/role.enum';
+
+// Tạo key cho role admin để k bị đánh cắp dữ liệu
+export const ROLES_KEY = 'roles';
+export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
